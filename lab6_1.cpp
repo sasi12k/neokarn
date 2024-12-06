b9531ae7cd506d8cce67b06f0e94cfb6cdeb4e4f
sasi12k <sasi12k@email.com>
2024-12-07 04:45:35 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int num; 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num; 
+    int even = 0, odd = 0; 
+    while(num != 0){  
+        if(num%2 == 0){ 
+            even = even+1; 
+        }else{
+            odd = odd+1;
+        }
+        cout << "Enter an integer: "; 
+        cin >> num; 
+    }
+    cout << "#Even numbers = " << even << "\n"; 
+    cout << "#Odd numbers = " << odd; 
     return 0;
 }
